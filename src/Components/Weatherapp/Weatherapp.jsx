e197a5930b14bd24c2bff1622a2f0cce75b1b020
Sina Persian <sinasasy4@gmail.com>
2024-03-03 22:44:47 -0800

    add a new feature

--- src/Components/Weatherapp/Weatherapp.jsx
+++ src/Components/Weatherapp/Weatherapp.jsx
@@ -11,56 +11,70 @@ import snow from '../Assets/snow.png'
 import rain from '../Assets/rain.png'
 
  const Weatherapp = () => {
-
   
   const apikey ="7867e5b0f752bfbc463bb28084ac0572";
-  const [wicon,setWicon] = useState(clearsky)
+  let [wicon,setWicon] = useState(clearsky)
+  let [result,setResult] = useState(false)
+   
+  let error = document.getElementsByClassName('error')
+  
+  const search = async()=>{
+    let location = document.getElementsByClassName('cityinput');
+    if(location[0].value=== ''){
+     setResult(false)
+      alert("you have to write something")
+      return 0;
+    }else{
+      setResult(true)
+        
+      
+    }
+    
+    let url = `https://api.openweathermap.org/data/2.5/weather?&units=metric&q=${location[0].value}&appid=${apikey}`
 
+  let response = await fetch(url);
 
-const search = async()=>{
-const element = document.getElementsByClassName('cityinput')
-if(element[0].value===""){
-  return 0;
-}
-
-let url = `https://api.openweathermap.org/data/2.5/weather?&units=metric&q=${element[0].value}&appid=${apikey}`
-
- let response = await fetch(url);
-  let data = await response.json();
-  const humidity = document.getElementsByClassName('humi')
-  const wind = document.getElementsByClassName('win')
-  const temper = document.getElementsByClassName('temper')
-  const location = document.getElementsByClassName('cityname')
-
-  humidity[0].innerHTML= data.main.humidity + "%";
-  wind[0].innerHTML = Math.floor(data.wind.speed) + " km/h";
-  temper[0].innerHTML = Math.floor(data.main.temp) + "°C";
-  location[0].innerHTML = data.name;
-
-  if(data.weather[0].icon === "01d" || data.weather[0].icon === "01n"){
-    setWicon(clearsky)
-  }else if(data.weather[0].icon === "02d" || data.weather[0].icon === "02n"){
-    setWicon(clouds)
-  }else if(data.weather[0].icon === "03d" || data.weather[0].icon === "03n"){
-    setWicon(clouds)
-  }else if(data.weather[0].icon === "04d" || data.weather[0].icon === "04n"){
-    setWicon(clouds)
-  }else if(data.weather[0].icon === "09d" || data.weather[0].icon === "09n"){
-    setWicon(drizzle)
-  }else if(data.weather[0].icon === "10d" || data.weather[0].icon === "10n"){
-    setWicon(rain)
-  }else if(data.weather[0].icon === "13d" || data.weather[0].icon === "13n"){
-    setWicon(snow)
+  if(response.status== 404){
+    error[0].style.display= 'block';
+    setResult(false)
+    return 0;
   }else{
-    setWicon(clearsky)
+    error[0].style.display= 'none';
   }
 
-  
+  let data = await response.json();
 
+  let temp = document.getElementsByClassName('temper')
+  let cityn = document.getElementsByClassName('cityname');
+  let humid = document.getElementsByClassName('humi');
+  let wind = document.getElementsByClassName('win');
+  
+  temp[0].innerHTML = Math.floor(data.main.temp) + "°C";
+  humid[0].innerHTML = data.main.humidity + " %";
+ wind[0].innerHTML = data.wind.speed + " km/h";
+ cityn[0].innerHTML = data.name;
+ 
+ if(data.weather[0].icon === '01d' || data.weather[0].icon === '01n'){
+  setWicon(clearsky)
+ }else if(data.weather[0].icon === "02d" || data.weather[0].icon === "02n"){
+  setWicon(clouds)
+}else if(data.weather[0].icon === "03d" || data.weather[0].icon === "03n"){
+  setWicon(clouds)
+}else if(data.weather[0].icon === "04d" || data.weather[0].icon === "04n"){
+  setWicon(clouds)
+}else if(data.weather[0].icon === "09d" || data.weather[0].icon === "09n"){
+  setWicon(drizzle)
+}else if(data.weather[0].icon === "10d" || data.weather[0].icon === "10n"){
+  setWicon(rain)
+}else if(data.weather[0].icon === "13d" || data.weather[0].icon === "13n"){
+  setWicon(snow)
+}else{
+  setWicon(clearsky)
 }
 
 
 
+  }
 
 
   return (
@@ -69,7 +83,9 @@ let url = `https://api.openweathermap.org/data/2.5/weather?&units=metric&q=${ele
       <input type="text" className='cityinput' placeholder='Search City' />
       <button onClick={()=>{search()}}><img src={searchicon}  /></button>
      </div>
-     <div className="wicon">
+     <p className='error'>you have to write a valid name</p>
+
+     {result?<div><div className="wicon">
       <img src={wicon} alt="" />
      </div>
      
@@ -94,7 +110,8 @@ let url = `https://api.openweathermap.org/data/2.5/weather?&units=metric&q=${ele
           <p>Wind Speed</p>
         </div>
       </div>
-     </div>
+     </div></div>:<div></div>}
+     
     </div>
   )
 }
